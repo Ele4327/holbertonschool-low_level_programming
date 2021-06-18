a6ac3fd0ff197e538fdb159e83a83cad9a4417ae
Ele4327 <3380@holbertonschool.com>
2021-06-17 21:26:57 -0500

    Write without others V.IV

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<unistd.h>
 
 /**
  * main - Entry Point
@@ -8,6 +9,6 @@
 
 int main(void)
 {
-    write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" 88);
+    write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",  88);
     return(1);
 }
