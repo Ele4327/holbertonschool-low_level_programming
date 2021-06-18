3625462bd7f4e73a66f0c9ab512ef55f6a36e92c
Ele4327 <3380@holbertonschool.com>
2021-06-17 20:00:36 -0500

    Size of bytes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,22 +1,23 @@
 #include<stdio.h>
 
 /**
- *  Start of function who starts calculate
- *  the size of the variables
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 
-int main(void) 
+int main(void)
 {
-    int intType;
-    float floatType;
-    double doubleType;
-    char charType;
+	int intType;
+	float floatType;
+	double doubleType;
+	char charType;
 
-    /* Size of evaluates the size of a variable */
-    printf("Size of int: %zu bytes\n", sizeof(intType));
-    printf("Size of float: %zu bytes\n", sizeof(floatType));
-    printf("Size of double: %zu bytes\n", sizeof(doubleType));
-    printf("Size of char: %zu byte\n", sizeof(charType));
-    
-    return 0;
+	/* Size of evaluates the size of a variable */
+	printf("Size of int: %zu bytes\n", sizeof(intType));
+	printf("Size of float: %zu bytes\n", sizeof(floatType));
+	printf("Size of double: %zu bytes\n", sizeof(doubleType));
+	printf("Size of char: %zu byte\n", sizeof(charType));
+
+	return (0);
 }
