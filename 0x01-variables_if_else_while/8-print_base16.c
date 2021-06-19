3877e5800971e06c252fe37dea703982e579d734
Ele4327 <3380@holbertonschool.com>
2021-06-18 22:01:01 -0500

    Base16WTFHolberton XD

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x;
+	char xz;
+
+	for (x = 48; x < 58; x++)
+
+	{
+		putchar(x);
+	}
+
+	for (xz = 'a'; xz <= 'f'; xz++)
+	{
+		putchar(xz);
+	}
+
+	return (0);
+}
