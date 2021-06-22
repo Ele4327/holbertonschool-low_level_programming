9f9409cbbdcdaa2c017f47133a9536f5a92f8ccd
Ele4327 <3380@holbertonschool.com>
2021-06-22 14:58:19 -0500

    Alphabet in lowercase ASCII V.II

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,19 @@
 #include "holberton.h"
 
+/**
+ * print_alphabet - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
 void print_alphabet(void)
 {
 
-    int x;
+	int x;
 
-    for (x = 0; x <= 25; x++)
-    {
-        _putchar(x + 97);
-    }
-    _putchar('\n');
+	for (x = 0; x <= 25; x++)
+	{
+		_putchar(x + 97);
+	}
+	_putchar('\n');
 }
