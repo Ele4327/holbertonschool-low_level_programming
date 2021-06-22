25e079701361340449ec5cca0c2f3a5088ff26fb
Ele4327 <3380@holbertonschool.com>
2021-06-21 23:43:21 -0500

    Yeah!

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main(void)
+
+{
+	int x;
+	int y;
+	int z;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0; y <= 9; y++)
+		{
+			if (y != x)
+			{
+				if (y > x)
+				{
+					for (z = 0; z <= 9; z++)
+					{
+						if (z != x)
+						{
+							if (z != y)
+							{
+								if (z > y)
+								{
+									putchar(x + 48);
+									putchar(y + 48);
+									putchar(z + 48);
+
+									if (!(z == 9 && y == 8 && x == 7))
+									{
+										putchar(',');
+										putchar(' ');
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	return (0);
+}
