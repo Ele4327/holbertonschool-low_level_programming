529bdc0ca57ef54dfcb0240d133aba8f981dcdbc
Ele4327 <3380@holbertonschool.com>
2021-06-22 20:05:17 -0500

    Absolute of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * _abs - check the code for Holberton School students.
+ *
+ * @x: Value of the incognit.
+ * @y: Value of the absolute of the incognit.
+ *
+ * Return: Always 0.
+ */
+
+int _abs(int x, int y)
+{
+
+	y = 0;
+
+	if (x > 0)
+	{
+		y = (x * 1);
+		return (y);
+	}
+	else if (x < 0)
+	{
+		y = (x * -1);
+		return (y);
+	}
+	return (0);
+}
