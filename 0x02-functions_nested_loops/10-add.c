a56a28093d7d195919e8b72b6bc227fbb9e405aa
Ele4327 <3380@holbertonschool.com>
2021-06-23 00:03:58 -0500

    2 int more

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * add - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+int add(int)
+{
+	int x;
+	int y;
+	int ttl;
+	ttl = x + y;
+	return (ttl);
+}
