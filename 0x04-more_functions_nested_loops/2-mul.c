f9507fc519c2ed97265aa8e832d00c2740030373
Ele4327 <3380@holbertonschool.com>
2021-06-25 23:44:12 -0500

    Multiplication incoming

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+* mul - tests function that prints a multiplication
+* @a: Val 1 a multiplicate
+* @b: Val 2 a muktiplicate
+* Return: 0
+*/
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = (a * b);
+
+	return (c);
+}
