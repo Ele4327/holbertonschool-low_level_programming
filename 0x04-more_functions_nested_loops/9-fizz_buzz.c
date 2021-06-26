2cd259d15f7b6fe5b8c338dbdaf447fa26cb3365
Ele4327 <3380@holbertonschool.com>
2021-06-26 02:39:36 -0500

    Until 100 - FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+* main - check the code for Holberton School students.
+*
+* Return: 0.
+*/
+
+int main(void)
+{
+	int x;
+	int y;
+
+	for (x = 1; x <= 100; x++)
+	{
+		if (((x % 3) == 0) && ((x % 5) == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if ((x % 3) == 0)
+		{
+			printf("Fizz ");
+		}
+		else if ((x % 5) == 0)
+		{
+			if (!(x < 101))
+			{
+				printf("Buzz ");
+			}
+			else
+			{
+				printf("Buzz ");
+			}
+		}
+		else
+		{
+			printf("%d ", x);
+		}
+	}
+
+	printf("\n");
+}
