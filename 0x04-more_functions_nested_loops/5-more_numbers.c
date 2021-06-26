cc35d243f996e7904d473e0129279172b57c5869
Ele4327 <3380@holbertonschool.com>
2021-06-25 23:56:06 -0500

    Multiple times a numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+* more_numbers - tests function that prints a list of numbers multiple times
+* Return: 0
+*/
+
+void more_numbers(void)
+{
+	int x;
+	int y;
+
+	for (y = 0; y < 9; y++)
+	{
+		for (x = 0; x <= 14; x++)
+		{
+			if (x >= 10)
+			{
+				_putchar(x / 10 + '0');
+			}
+			_putchar(x % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
