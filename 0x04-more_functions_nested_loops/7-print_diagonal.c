d565c04a2c196abae30a6a56cd0ab6454b528f09
Ele4327 <3380@holbertonschool.com>
2021-06-26 00:23:06 -0500

    Diagon Alley!

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+* print_diagonal - tests function that prints a ceraintly way of a line
+* @n: Value of spaces an rows
+* Return: 0
+*/
+
+void print_diagonal(int n)
+{
+	int x;
+	int y;
+
+	for (x = 0; x <= n; x++)
+	{
+		for (y = 0; y < x; y++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
