a3fee509266e5bbb535b3b42cb432593542067a1
Ele4327 <3380@holbertonschool.com>
2021-06-30 00:21:51 -0500

    Half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+* puts_half - Function that calculate half of a string
+* @str: Pointer that point a place of the string
+* return: 0;
+*/
+
+void puts_half(char *str)
+{
+	int len, x, y;
+
+	for (len = 0; str[len] != '\0'; len++)
+	{
+	}
+
+	if ((len % 2) == 0)
+	{
+		x = (len / 2);
+		for (y = x; y < len; y++)
+		{
+			putchar(str[y]);
+		}
+	}
+	else
+	{
+		x = ((len - 1) / 2);
+		for (y = x + 1; y < len; y++)
+		{
+			putchar(str[y]);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
