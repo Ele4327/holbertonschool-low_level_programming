79f70eb2e3386c8bd1d687f6d6f963d779ab7e54
Ele4327 <3380@holbertonschool.com>
2021-06-29 20:35:00 -0500

    Only pairs

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+* puts2 - tests function that prints a string every other value
+* @str: pointer of str.
+* Return: 0
+*/
+
+void puts2(char *str)
+{
+
+	int x;
+
+	x = 0;
+	while (x < 8)
+	{
+		x++;
+		_putchar(*str);
+		str = str + 2;
+	}
+	_putchar('\n');
+}
