91231447671a3df12079b34d9470989126f75fc6
Ele4327 <3380@holbertonschool.com>
2021-06-29 22:47:10 -0500

    Change N

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* reset_to_98 - function print a triangle with certains values.
+* @n: N is the value to change
+* Return: 0.
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
