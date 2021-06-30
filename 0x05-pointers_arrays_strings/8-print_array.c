5f2c0e9d1955c2386b025d5a7367ec962bd6d6e5
Ele4327 <3380@holbertonschool.com>
2021-06-30 15:04:33 -0500

    Print a not Pointer?, or yes? XD

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,17 +1,25 @@
 #include <stdio.h>
 #include "holberton.h"
 
+/**
+* print_array - Function that calculate half of a string
+* @a: Pointer that point a place of the string
+* @n: Value of lenght of the array, number of the values
+* return: 0;
+*/
+
 void print_array(int *a, int n)
 {
 	int x;
 
 	for (x = 0; x < n; x++)
 	{
-		if (x != n)
+		printf("%d", a[x]);
+
+		if ((x + 1) != n)
 		{
 			printf(", ");
 		}
-		printf("%d", a[x]);
 	}
 	printf("\n");
 }
