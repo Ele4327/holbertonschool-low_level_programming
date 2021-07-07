ada5a6f36b2164ebbc871fedb757f76e5f4c5b5a
Ele4327 <3380@holbertonschool.com>
2021-07-07 06:26:43 -0500

    Print a String in Reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -3,28 +3,6 @@
 #include "holberton.h"
 
 /**
- * _puts_recursion - function that prints a string, followed by a new line.
- * @s: String base
- * Return: Always 0.
- */
-
-void _puts_recursion(char *s)
-{
-	if (*s == '\0')
-	{
-		return;
-	}
-	else
-	{
-		(_puts_recursion(s + 1));
-		if (*s != '\0')
-		{
-			_putchar(*s);
-		}
-	}
-}
-
-/**
  * _print_rev_recursion - function that prints a string in reverse.
  * @s: String base
  * Return: Always 0.
@@ -32,6 +10,16 @@ void _puts_recursion(char *s)
 
 void _print_rev_recursion(char *s)
 {
-	_puts_recursion(s);
-	putchar('\n');
+	if (*s == '\0')
+	{
+		return;
+	}
+	else
+	{
+		(_print_rev_recursion(s + 1));
+		if (*s != '\0')
+		{
+			_putchar(*s);
+		}
+	}
 }
