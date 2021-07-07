134af39fe94c04e623be9fc86b344fa9524e63bf
Ele4327 <3380@holbertonschool.com>
2021-07-07 05:36:54 -0500

    Print with Recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * _puts_recursion - function that prints a string, followed by a new line.
+ * @s: String base
+ * Return: Always 0.
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	else
+	{
+		putchar(*s);
+		return (_puts_recursion(++s));
+	}
+}
