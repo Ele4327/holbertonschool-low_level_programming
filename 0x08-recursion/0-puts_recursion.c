0824e04fb15fa265fe5c17ecf15c82916c2d7bea
Ele4327 <3380@holbertonschool.com>
2021-07-07 05:54:28 -0500

    Print a String With Recursion V.III

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -12,11 +12,12 @@ void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
+		_putchar('\n');
 		return;
 	}
 	else
 	{
 		_putchar(*s);
-		return (_puts_recursion(++s));
+		_puts_recursion(++s);
 	}
 }
