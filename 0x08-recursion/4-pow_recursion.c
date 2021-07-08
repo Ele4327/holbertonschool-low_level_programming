5cd59dccdea543c6d6120dd877127f781c96c177
Ele4327 <3380@holbertonschool.com>
2021-07-07 19:39:07 -0500

    Exponent

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _pow_recursion - returns the value of x raised to the power of y.
+ * @x: Number Base.
+ * @y: Exponent.
+ * Return: A variable type int.
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+
+	if (y == 0)
+	{
+		return (1);
+	}
+
+	return (x * _pow_recursion(x, y - 1));
+}
