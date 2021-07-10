b23c0a876cb761d3096534a8b0e193e02fc7ea4f
Ele4327 <3380@holbertonschool.com>
2021-07-09 22:25:42 -0500

    Print all arguments

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - Print ALL arguments
+ * @argc: Counter
+ * @argv: Vector
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+    int x;
+
+    for (x = 0; x < argc; x++)
+    {
+        printf("%sn", argv[x]);
+    }
+    return (0);
+}
