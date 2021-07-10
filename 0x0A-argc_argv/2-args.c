c789907dd385044a2c210508e215019f780f5acd
Ele4327 <3380@holbertonschool.com>
2021-07-09 22:30:58 -0500

    Print all arguments V.II

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -8,13 +8,13 @@
  * Return: Always 0.
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
     int x;
 
     for (x = 0; x < argc; x++)
     {
-        printf("%sn", argv[x]);
+        printf("%s\n", argv[x]);
     }
     return (0);
 }
