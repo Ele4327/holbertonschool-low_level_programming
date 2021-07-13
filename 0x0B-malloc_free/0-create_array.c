0c207f7494339187e7438a990a991b7df2002706
Ele4327 <3380@holbertonschool.com>
2021-07-13 03:29:00 -0500

    Create a Array and initialize a specific char

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+void array(char *ptrarray, char o, unsigned int size);
+
+/**
+ * *create_array - Creation of a Array
+ * @c: A specific character
+ * @size: Value to mide the array
+ * Return: The value stored
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *x;
+
+	x = (char *)malloc(sizeof(char) * size);
+	array(x, c, size);
+	return (x);
+}
+
+/**
+ * array - Creation of a Array
+ * @ptrarray: Pointer of the array
+ * @o: A char value
+ * @size: Size determined of the string
+ * return: Always 0
+ */
+
+void array(char *ptrarray, char o, unsigned int size)
+{
+	while (ptrarray && size--)
+	{
+		*ptrarray++ = o;
+	}
+}
