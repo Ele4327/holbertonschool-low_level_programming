a117dac43348a37336c3a8d79a3becec2bc0dea5
Ele4327m <3380@holbertonschool.com>
2021-07-14 08:50:47 -0500

    Concatenate All Arguments

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,57 +1,50 @@
-#include <stdlib.h>
-#include <string.h>
 #include "holberton.h"
 
 /**
- * argstostr - Concatenates all the arguments of your program.
- * @ac: Array of counters
- * @av: Size of Array of counters
- * Return: Always 0.
+ * strtow - splits strings to words
+ * @str: string to split
+ * Return: pointer to an array of words
  */
 
-void copy(char *Buffer, char **av, int ac)
+char **strtow(char *str)
 {
+	char **array;
+	int i, j, count, len, k, m;
 
-	while (ac--)
+	count = k = 0;
+	if (str == NULL || str[0] == '\0')
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
 	{
-
-		while (**av != '\0')
+		if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0'))
+			count++;
+	}
+	if (count == 0)
+		return (NULL);
+	array = malloc(((count + 1) * sizeof(char *)));
+	if (array == NULL)
+		return (NULL);
+	for (i = 0; str[i] != '\0' && k < count; i++)
+	{
+		if (str[i] != ' ')
 		{
-			*Buffer++ = *((*av)++);
+			len = 0;
+			j = i;
+			while (str[j] != ' ' && str[j] != '\0')
+				j++, len++;
+			array[k] = malloc((len + 1) * sizeof(char));
+			if (array[k] == NULL)
+			{
+				for (k = k - 1; k >= 0; k++)
+					free(array[k]);
+				free(array);
+				return (NULL);
+			}
+			for (m = 0; m < len; m++, i++)
+				array[k][m] = str[i];
+			array[k++][m] = '\0';
 		}
-		*Buffer++ = '\n';
-		av++;
 	}
-	*Buffer = '\0';
-}
-
-char *argstostr(int ac, char **av)
-{
-
-	if (ac && av)
-	{
-		int result = 0;
-		char *Buffer = NULL;
-		int temp = ac;
-		char **temp1 = av;
-
-		while (ac--)
-		{
-
-			result += strlen(*av++);
-			result++;
-		}
-		result++;
-
-		Buffer = (char *)malloc(sizeof(char) * result);
-
-		if (Buffer)
-			copy(Buffer, temp1, temp);
-
-		return Buffer;
-	}
-	else
-	{
-		return NULL;
-	}
+	array[k] = NULL;
+	return (array);
 }
