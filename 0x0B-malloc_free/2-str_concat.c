268b6efd5fca35b172cc693edb2857671ca04e80
Ele4327m <3380@holbertonschool.com>
2021-07-14 02:21:00 -0500

    Concatenate Strings V.II

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,129 +1,158 @@
-/**
- * copyS - Function of copy string
- * @buffer: Space to copy string
- * @str: The string copied
- * Return: Always 0
- */
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
 
-void copyS(char *buffer, char *str)
-{
-	if (!*str)
-	{
-		;
-	}
-	else
-	{
-
-		*buffer = *str;
-		copyS(buffer + 1, str + 1);
-	}
-}
+int len(char *str);
+int lenR(char *str, int counter);
+void copy(char *dest, char *src1, char *src2);
+void copyS(char *dest, char *src1);
 
 /**
- * copy - Function of copy string
- * @buffer: Space to copy string
- * @str1: The string 1 copied
- * @str2: The string 2 copied
- * Return: Always 0
- */
-
-void copy(char *buffer, char *str1, char *str2)
-{
-
-	if (!*str1 && *!str2)
-	{
-		;
-	}
-	else
-	{
-
-		if (*str1)
-			*buffer++ = *str1++;
-		else if (*str2)
-			*buffer++ = *str2++;
-
-		copyS(buffer + 1, str1 + 1, str2 + 1);
-	}
-}
-
-/**
- * length - Size of a string
- * @s1: The string 1 copied
- * @counter: Counter to determine the size of the string
- * Return: Always 0
- */
-
-int length(char *s1, int counter)
-{
-	if (!*s1)
-	{
-		return (counter);
-	}
-	else
-	{
-		return (lenght(s1 + 1, counter + 1));
-	}
-}
-
-/**
- * len - Function to determine the size of the string
- * @str: The string copied
- * Return: Type int
- */
-
-int len(char *str)
-{
-
-	return (length(s1, 0));
-}
-
-/**
- * str_concat - Function that determines the conditions of the concatenation
- * @s1: The string 1 copied
- * @s2: The string 2 copied
- * Return: String that concatenates
+ * str_concat - function that returns a pointer to a newly allocated memory.
+ * @s1: The size of the array to be initialized.
+ * @s2: The size of the array 2
+ * Return: If size == 0 or the function fails - NULL.
+ *         Otherwise - a pointer to the array.
  */
 
 char *str_concat(char *s1, char *s2)
 {
 	if (s1 && !s2)
 	{
-		int lenght;
+		int lenght1 = len(s1);
+		char *buffer = (char *)malloc(sizeof(char) * (lenght1 + 1));
 
-		lenght = len(s1) int *buffer = (char *)malloc(sizeof(char) * (lenght + 1));
 		if (buffer)
 		{
-			copy(buffer, s1);
-			buffer[lenght + 1] = '\0';
+
+			copyS(buffer, s1);
+			buffer[lenght1 + 1] = '\0';
 		}
+
 		return (buffer);
 	}
 	else if (s2 && !s1)
 	{
-		int lenght;
+		int lenght1 = len(s2);
+		char *buffer = (char *)malloc(sizeof(char) * (lenght1 + 1));
 
-		lenght = len(s2) int *buffer = (char *)malloc(sizeof(char) * (lenght + 1));
 		if (buffer)
 		{
-			copy(buffer, s2), buffer[lenght + 1] = '\0';
+
+			copyS(buffer, s2);
+			buffer[lenght1 + 1] = '\0';
 		}
+
 		return (buffer);
 	}
 	else if (s1 && s2)
 	{
-		int lenght1 = len(s1), int lenght2 = len(s2);
-		int *buffer = (char *)malloc(sizeof(char) * (lenght1 + lenght2 + 1));
+		int lenght1 = len(s1);
+		int lenght2 = len(s2);
+		char *buffer = (char *)malloc(sizeof(char) * (lenght1 + lenght2 + 1));
 
 		if (buffer)
 		{
-			copy(buffer, s1, s2), buffer[lenght1 + lenght2 + 1] = '\0';
+
+			copy(buffer, s1, s2);
+			buffer[lenght2 + 1 + lenght1] = '\0';
 		}
+
 		return (buffer);
 	}
 	else
 	{
-		char *buffer = (char *)malloc(sizeof(char) * 1);
+		char *buffer = (char *)malloc(sizeof(char *) * 1);
 
 		buffer[0] = '\0';
+
+		return (buffer);
+	}
+}
+/**
+ * len - Functions copy values
+ * @str: Destine of de buffer.
+ * Return: If size == 0 or the function fails - NULL.
+ *         Otherwise - a pointer to the array.
+ */
+
+int len(char *str)
+{
+	return (lenR(str, 0));
+}
+
+/**
+ * lenR - Functions copy values
+ * @str: Destine of de buffer.
+ * @counter: addres of the string search.
+ * Return: If size == 0 or the function fails - NULL.
+ *         Otherwise - a pointer to the array.
+ */
+
+int lenR(char *str, int counter)
+{
+
+	if (!(*str))
+	{
+		return (counter);
+	}
+	else
+	{
+		return (lenR(str + 1, counter + 1));
+	}
+}
+
+/**
+ * copyS - Functions copy values
+ * @dest: Destine of de buffer.
+ * @src1: addres of the string search
+ * Return: If size == 0 or the function fails - NULL.
+ *         Otherwise - a pointer to the array.
+ */
+void copyS(char *dest, char *src1)
+{
+
+	if (!*src1)
+	{
+		;
+	}
+	else
+	{
+
+		if (*src1)
+		{
+			*dest++ = *src1++;
+		}
+		copyS(dest, src1);
+	}
+}
+
+/**
+ * copy - Functions copy values
+ * @dest: Destine of de buffer.
+ * @src1: addres of the string search.
+ * @src2: addres of the string 2
+ * Return: If size == 0 or the function fails - NULL.
+ *         Otherwise - a pointer to the array.
+ */
+void copy(char *dest, char *src1, char *src2)
+{
+
+	if (!*src1 && !*src2)
+	{
+		;
+	}
+	else
+	{
+
+		if (*src1)
+		{
+			*dest++ = *src1++;
+		}
+		else if (*src2)
+		{
+			*dest++ = *src2++;
+		}
+		copy(dest, src1, src2);
 	}
 }
