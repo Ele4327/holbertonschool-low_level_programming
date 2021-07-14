487eb65591f0784d66ae61b265d53026a68a60a3
Ele4327m <3380@holbertonschool.com>
2021-07-14 06:41:45 -0500

    Return a Pointer 2D

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -3,34 +3,6 @@
 #include "holberton.h"
 
 /**
- * print_grid - function that print a grid of aa 2 dimensional pointer.
- * @grid: The grid of the array
- * @width: Width of the array 2d
- * @height: Height of the array 2d
- * Return: If size == 0 or the function fails - NULL.
- *         Otherwise - a pointer to the array.
- */
-
-void print_grid(int **grid, int width, int height)
-{
-	int w;
-	int h;
-
-	h = 0;
-	while (h < height)
-	{
-		w = 0;
-		while (w < width)
-		{
-			printf("%d ", grid[h][w]);
-			w++;
-		}
-		printf("%s", "\n");
-		h++;
-	}
-}
-
-/**
  * init - function that initialize.
  * @argv: Argument Vector
  * @width: Width of the array 2d
@@ -38,7 +10,7 @@ void print_grid(int **grid, int width, int height)
  *         Otherwise - a pointer to the array.
  */
 
-int init(int **argv, int width)
+void init(int **argv, int width)
 {
 
 	int copywidth = 0;
@@ -95,28 +67,4 @@ int **alloc_grid(int width, int height)
 	init(argv1, width);
 
 	return (argv1);
-}
-
-/**
- * main - function main.
- * Return: If size == 0 or the function fails - NULL.
- *         Otherwise - a pointer to the array.
- */
-
-int main(void)
-{
-	int **grid;
-
-	grid = alloc_grid(6, 4);
-	if (grid == NULL)
-	{
-		return (1);
-	}
-	print_grid(grid, 6, 4);
-	grid[0][3] = 98;
-	grid[3][4] = 402;
-	printf("%s", "\n");
-
-	print_grid(grid, 6, 4);
-	return (0);
 }
