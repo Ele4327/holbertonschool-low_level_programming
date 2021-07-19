d621d9fe6376a8131ce6e6b48fee989a18f299c9
L5438 <sebas.4327@hotmail.com>
2021-07-19 12:33:03 -0500

    Dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef holberton
+#define holberton
+
+/**
+ * struct dog - Estructure of data who contains data of a dog
+ * @name: member name of the dog
+ * @age: member age of the dog
+ * @owner: member owner of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
