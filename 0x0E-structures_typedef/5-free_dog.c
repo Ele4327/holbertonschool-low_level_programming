7eed17d1d409876f976263e74ba5400e057e2cfe
L5438 <sebas.4327@hotmail.com>
2021-07-19 17:59:36 -0500

    Free the Dogsvalgrind ./f

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -3,12 +3,15 @@
 #include "dog.h"
 
 /**
- * free_dog - Free the dogs!
+ * free_dog: Free the dogs!
  * @d: Variable of data type
  * Return: Always 0
  */
 
 void free_dog(dog_t *d)
 {
-	free(d);
+	if (d)
+	{
+		free(d);
+	}
 }
