9627fe65bde48bd3f46331a593231d6537c6970b
L5438 <sebas.4327@hotmail.com>
2021-07-19 17:39:26 -0500

    Print a Dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -17,7 +17,7 @@ void print_dog(struct dog *d)
 		}
 		else
 		{
-			printf("Name %s\n", (*d).name);
+			printf("Name: %s\n", (*d).name);
 		}
 
 		printf("Age: %f\n", d->age);
@@ -28,7 +28,7 @@ void print_dog(struct dog *d)
 		}
 		else
 		{
-			printf("Owner %s\n", d->owner);
+			printf("Owner: %s\n", d->owner);
 		}
 	}
 }
