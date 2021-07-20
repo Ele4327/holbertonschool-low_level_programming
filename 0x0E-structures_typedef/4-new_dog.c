0a9a3ab8a154fe55a4e71a73d716b8789835b6a9
L5438 <sebas.4327@hotmail.com>
2021-07-19 19:16:00 -0500

    Create a New Dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,23 +2,56 @@
 #include <stdio.h>
 #include "dog.h"
 
+int lenpntr(char *data);
+void flldt(char *data_src, char *data_dest);
+
 /**
  * init_dog - Initialize function dog from dog.h
  * @d: Name of the structure
  * @name: Member name of the dog
  * @age: Member age of the dog
  * @owner: Member owner of the dog
+ * Return: Value of initialice dog
  */
 
-void init_dog(struct dog *d, char *name, float age, char *owner)
+int init_dog(struct dog *d, char *name, float age, char *owner)
 {
 
-	if (d)
+	if (name)
 	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
+		int x;
+
+		x = lenpntr(name);
+		d->name = malloc(sizeof(char) * x);
+
+		if (d->name == NULL)
+		{
+			return (0);
+		}
+
+		flldt(name, d->name);
+		(d->name)[x] = '\0';
 	}
+
+	if (owner)
+	{
+		int x;
+
+		x = lenpntr(owner);
+		d->owner = malloc(sizeof(char) * x);
+
+		if (d->owner == NULL)
+		{
+			free(d->name);
+			return (0);
+		}
+
+		flldt(owner, d->owner);
+		(d->owner)[x - 1] = '\0';
+	}
+
+	d->age = age;
+	return (1);
 }
 
 /**
@@ -34,20 +67,56 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	dog_t *ptrmmry;
 
-	if (name == NULL || owner == NULL)
-	{
-		return (NULL);
-	}
-
 	ptrmmry = malloc(sizeof(dog_t));
 	if (ptrmmry == NULL)
 	{
-		free(ptrmmry);
 		return (NULL);
 	}
 	else
 	{
-		init_dog(ptrmmry, name, age, owner);
+		int status;
+
+		status = init_dog(ptrmmry, name, age, owner);
+
+		if (status == 0)
+		{
+			free(ptrmmry);
+			ptrmmry = NULL;
+		}
+
 		return (ptrmmry);
+	}
+}
+
+/**
+ * lenpntr - Lenght of a member of the structure
+ * @data: Member of the dog
+ * Return: Lenght of a member data
+ */
+
+int lenpntr(char *data)
+{
+	int cntdr = 0;
+
+	while (*data != '\0')
+	{
+		cntdr++;
+		data++;
+	}
+	return (cntdr);
+}
+
+/**
+ * flldt - Lenght of a member of the structure
+ * @data_src: Data Source
+ * @data_dest: Data Destiny
+ * Return: Always 0
+ */
+
+void flldt(char *data_src, char *data_dest)
+{
+	while (*data_src != '\0')
+	{
+		*data_dest++ = *data_src++;
 	}
 }
