cc682b900a8c9bac99a137300e8151af75001f2f
Ele4327 <3380@holbertonschool.com>
2021-07-21 23:53:08 -0500

    Operators Functions

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - Function add.
+ * @number1: First value number.
+ * @number2: Second value number.
+ * Return: Always 0.
+ */
+
+int op_add(int number1, int number2)
+{
+	return (number1 + number2);
+}
+
+/**
+ * op_sub - Function subtract.
+ * @number1: First value number.
+ * @number2: Second value number.
+ * Return: Always 0.
+ */
+
+int op_sub(int number1, int number2)
+{
+	return (number1 - number2);
+}
+
+/**
+ * op_mul - Function multiply.
+ * @number1: First value number.
+ * @number2: Second value number.
+ * Return: Always 0.
+ */
+
+int op_mul(int number1, int number2)
+{
+	return (number1 * number2);
+}
+
+/**
+ * op_div - Function divide.
+ * @number1: First value number.
+ * @number2: Second value number.
+ * Return: Always 0.
+ */
+
+int op_div(int number1, int number2)
+{
+	return (number1 / number2);
+}
+
+/**
+ * op_mod - Function modulus
+ * @number1: First value number.
+ * @number2: Second value number.
+ * Return: Always 0.
+ */
+
+int op_mod(int number1, int number2)
+{
+	return (number1 % number2);
+}
