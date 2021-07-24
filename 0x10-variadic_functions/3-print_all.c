2dad249ffb31df5024ae4bc269e2afad9022b6fb
Ele4327 <3380@holbertonschool.com>
2021-07-23 23:47:57 -0500

    Print Everything1git variadic_functions.h Header.h1git variadic_functions.h Header.h1git variadic_functions.h Header.h1git variadic_functions.h Header.h Indexed

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <string.h>
+#include "variadic_functions.h"
+
+void print_c(va_list list_arg);
+void print_f(va_list list_arg);
+void print_i(va_list list_arg);
+void print_s(va_list list_arg);
 
 /**
  * print_all - Function that prints anything
@@ -10,44 +16,89 @@
 
 void print_all(const char *const format, ...)
 {
-	char *ptr = NULL;
-	const char *fmt = (const char *)format;
-	const char *fmt1 = (const char *)format + strlen(format) - 1;
-	int len = strlen(format) - 1;
-	va_list list;
 
-	va_start(list, format);
-	while (fmt && *fmt)
+	int x = 0, z = 0;
+	char *str = "";
+
+	print_all_t array[] = {
+	    {"c", print_c},
+	    {"f", print_f},
+	    {"i", print_i},
+	    {"s", print_s},
+	    {NULL, NULL}};
+
+	va_list list_arg;
+
+	va_start(list_arg, format);
+
+	while (format && format[x])
 	{
-		switch (*fmt)
+		z = 0;
+		while (array[z].format)
 		{
-		case 'c':
-			printf("%c", va_arg(list, int)), fmt++;
-			break;
-		case 's':
-			ptr = va_arg(list, char *);
-			if (ptr == NULL)
-				ptr = "(nil)";
-			printf("%s", ptr), fmt++;
-			break;
-		case 'i':
-			printf("%d", va_arg(list, int)), fmt++;
-			break;
-		case 'f':
-			printf("%f", va_arg(list, double)), fmt++;
-			break;
-		}
-		while (*fmt)
-		{
-			if ((*fmt == 'c' || *fmt == 's' || *fmt == 'i' || *fmt == 'f') &&
-			    (fmt <= fmt1) && (len > 1))
+			if (*(array[z].format) == format[x])
 			{
-				printf(", ");
+				printf("%s", str);
+				array[z].f(list_arg);
+				str = ", ";
 				break;
 			}
-			fmt++;
+			z++;
 		}
+		x++;
 	}
 	printf("\n");
-	va_end(list);
+}
+
+/**
+ * print_c - Function that prints type data Char
+ * @list_arg: List of Arguments in Entry Point
+ * Return: Always 0.
+ */
+
+void print_c(va_list list_arg)
+{
+	printf("%c", va_arg(list_arg, int));
+}
+
+/**
+ * print_s - Function that prints type data Char (Strings)
+ * @list_arg: List of Arguments in Entry Point
+ * Return: Always 0.
+ */
+
+void print_s(va_list list_arg)
+{
+	char *str = va_arg(list_arg, char *);
+
+	if (str == NULL)
+	{
+		printf("(nil)");
+	}
+	else
+	{
+		printf("%s", str);
+	}
+}
+
+/**
+ * print_f - Function that prints type data Float
+ * @list_arg: List of Arguments in Entry Point
+ * Return: Always 0.
+ */
+
+void print_f(va_list list_arg)
+{
+	printf("%f", va_arg(list_arg, double));
+}
+
+/**
+ * print_i - Function that prints type data Integer
+ * @list_arg: List of Arguments in Entry Point
+ * Return: Always 0.
+ */
+
+void print_i(va_list list_arg)
+{
+	printf("%d", va_arg(list_arg, int));
 }
