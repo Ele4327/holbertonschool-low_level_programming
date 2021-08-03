522b772ce512caf1604229f6cbc8892513b20231
Ele4327 <3380@holbertonschool.com>
2021-08-03 16:19:35 -0500

    All elmeents

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_listint - Print all elements of a list
+ * @h: Pointer
+ * Return: Data type size_t
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t x;
+	/* x is size of all elements */
+
+	size_t x = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", (*h).n);
+		x++;
+		h = (*h).next;
+	}
+
+	return (x);
+}
