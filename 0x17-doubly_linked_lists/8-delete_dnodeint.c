252044644552f5c07ac0efa3d6eb93405d7ccec4
Ele4327 <3380@holbertonschool.com>
2021-09-17 23:03:31 -0500

    Delete a node

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,54 +1,52 @@
 #include "lists.h"
 
 /**
- * delete_dnodeint_at_index: Delete a node in a specific position
- *@header: Pointer to the header.
+ * delete_dnodeint_at_index - Delete a node in a specific position
+ *@head: Pointer to the header.
  *@index: Position of the node a delete
  * Return: -1 if faled, 1 if succes
  */
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    dlistint_t *x = *head;
-    unsigned int z = 0;
+	dlistint_t *x = *head;
+	unsigned int z = 0;
 
-    if (x == NULL)
-    {
-        return (-1);
-    }
+	if (x == NULL)
+	{
+		return (-1);
+	}
 
-    if (index == 0)
-    {
-        *head = x->next;
+	if (index == 0)
+	{
+		*head = x->next;
 
-        if (x->next != NULL)
-        {
-            x->next->prev = NULL;
-        }
+		if (x->next != NULL)
+		{
+			x->next->prev = NULL;
+		}
+		free(x);
 
-        free(x);
+		return (1);
+	}
 
-        return (1);
-    }
+	for (z = 0; z < index; z++)
+	{
+		if (x->next == NULL)
+		{
+			return (-1);
+		}
 
-    for (z = 0; z < index; z++)
-    {
-        if (x->next == NULL)
-        {
-            return (-1);
-        }
+		x = x->next;
+	}
 
-        x = x->next;
-    }
+	x->prev->next = x->next;
 
-    x->prev->next = x->next;
+	if (x->next != NULL)
+	{
+		x->next->prev = x->prev;
+	}
+	free(x);
 
-    if (x->next != NULL)
-    {
-        x->next->prev = x->prev;
-    }
-
-    free(x);
-
-    return (1);
+	return (1);
 }
