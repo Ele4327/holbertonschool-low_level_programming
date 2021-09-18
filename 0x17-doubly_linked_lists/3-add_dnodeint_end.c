03f8779e09a1c64b4cbd7d74edf9366a7d2de92b
Ele4327 <3380@holbertonschool.com>
2021-09-17 23:05:56 -0500

    Add a Node

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -9,34 +9,34 @@
 
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-    dlistint_t *node_x = NULL;
-    dlistint_t *z = NULL;
+	dlistint_t *node_x = NULL;
+	dlistint_t *z = NULL;
 
-    z = *head;
-    node_x = malloc(sizeof(dlistint_t));
+	z = *head;
+	node_x = malloc(sizeof(dlistint_t));
 
-    if (node_x == NULL)
-    {
-        return (NULL);
-    }
+	if (node_x == NULL)
+	{
+		return (NULL);
+	}
 
-    node_x->n = n;
-    node_x->next = NULL;
+	node_x->n = n;
+	node_x->next = NULL;
 
-    if (*head == NULL)
-    {
-        node_x->prev = NULL;
-        *head = node_x;
-        return (node_x);
-    }
+	if (*head == NULL)
+	{
+		node_x->prev = NULL;
+		*head = node_x;
+		return (node_x);
+	}
 
-    while (z->next != NULL)
-    {
-        z = z->next;
-    }
+	while (z->next != NULL)
+	{
+		z = z->next;
+	}
 
-    z->next = node_x;
-    node_x->prev = z;
+	z->next = node_x;
+	node_x->prev = z;
 
-    return (node_x);
+	return (node_x);
 }
