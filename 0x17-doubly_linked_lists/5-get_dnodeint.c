a2ec0fba7c61f357e54fb58e80d041c9d333ece8
Ele4327 <3380@holbertonschool.com>
2021-09-17 22:02:38 -0500

    Return a Node

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -13,7 +13,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
         {
             return (NULL);
         }
-        z = z - x > next;
+        z = z->next;
         x++;
     }
 
