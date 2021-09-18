55eef0aca2b799a153daa8ebf40237045edf35b2
Ele4327 <3380@holbertonschool.com>
2021-09-17 22:31:42 -0500

    Get Total

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - Total of all info of a linked list
+ * @head: Pointer to header.
+ * Return: Total of all info of a linked list
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *z = NULL;
+	int plus = 0;
+
+	if (head == NULL)
+	{
+		return (0);
+	}
+
+	z = head;
+
+	while (z)
+	{
+		plus += z->n;
+		z = z->next;
+	}
+
+	return (plus);
+}
