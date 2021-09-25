a7fb2b1a13a2bc445622e6e7ae410b5ec736b956
Ele4327 <3380@holbertonschool.com>
2021-09-25 16:03:06 -0500

    Update

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,16 +1,23 @@
 #include "lists.h"
 
+/**
+ * print_dlistint - Print a list from a double list.
+ * @h: A ponter to the header of the file.
+ * Return: Number of elements on the list
+ */
+
 size_t print_dlistint(const dlistint_t *h)
 {
-    if (h)
-    {
-        int x;
-        for (x = 0; h != NULL; x++)
-        {
-            printf("%d\n", h->n);
-            h = h->next;
-        }
-        return (x);
-    }
-    return (0);
+	if (h)
+	{
+		int x;
+
+		for (x = 0; h != NULL; x++)
+		{
+			printf("%d\n", h->n);
+			h = h->next;
+		}
+		return (x);
+	}
+	return (0);
 }
